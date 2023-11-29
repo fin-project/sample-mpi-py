f18c5c6e7f5c4b6eba71122f44f6a0df5b1c2716
cys <yokesan@asb.com.my>
2023-11-29 23:35:09 +0800

    pag integration

--- project/__init__.py
+++ project/__init__.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mpi_test_secret'
 app.config['KEY_PATH'] = "./key"
 
-app.config["TEST_MPI_URL"] = "tx"
 app.config['TEST_MID'] = "000000000000033"
-app.config["TEST_APP_KEY"] = "txid"
-app.config['MPI_URL'] = "https://devlink.paydee.co/mpi"
+app.config['MPI_URL'] = "https://devlinkv2.paydee.co/mpigw"
 
 from project import routes
